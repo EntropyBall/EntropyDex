19169ccd22ba2c880e032746b24807ebfba63866
EntropyBall <everywhere.stretch@gmail.com>
2022-12-15 19:54:57 +0100

    Resize images with ratio

--- client/src/PokemonItem.js
+++ client/src/PokemonItem.js
@@ -5,13 +5,15 @@ const PokemonItem = ({ pokemon }) => {
     // USE LOADING LAZY REACT https://www.youtube.com/watch?v=8viWcH5bUE4
     return (
         <div className={`item ${pokemon.family}`}>
+
             {(pokemon.images.length &&
                 <LazyLoadImage
+                    className='pokemonImg'
                     src={"http://localhost:3001/images/Pokemon/" + pokemon.images[0].filename}
                     // src={"/images/Pokemon/" + pokemon.images[0].filename}
                     alt="pokemon.images[1]"
                 />)}
-            {pokemon.dexid} - {pokemon.name} {pokemon.region}
+            {/* {pokemon.dexid} - {pokemon.name} {pokemon.region} */}
         </div>
     )
 }
