f3598c063edf09ae8b5869d12d8eae0377d90e6b
EntropyBall <everywhere.stretch@gmail.com>
2022-12-27 23:29:26 +0100

    Display proper img

--- client/src/PokemonItem.js
+++ client/src/PokemonItem.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { LazyLoadImage } from "react-lazy-load-image-component"
 
 const PokemonItem = ({ pokemon }) => {
-    const baseForm = pokemon.images.find(image => image.filename.split(".").length === 3)
-    console.log(baseForm)
+    const baseForm = pokemon.images.find(image => !image.filename.includes('.s.'))
     const baseFormURL = baseForm ?
         "http://localhost:3001/images/Pokemon/" + baseForm.filename :
         ""
@@ -11,7 +10,7 @@ const PokemonItem = ({ pokemon }) => {
     // USE LOADING LAZY REACT https://www.youtube.com/watch?v=8viWcH5bUE4
     return (
         <div className={`item ${pokemon.family}`}>
-            <p className='name'>{pokemon.name}</p>
+            <p className='name'>{pokemon.name}-{pokemon.images.length}</p>
             {(baseForm &&
                 <LazyLoadImage
                     className='pokemonImg'
